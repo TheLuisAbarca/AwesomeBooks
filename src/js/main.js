d0dc5d298e5540734e8074b162717d80aee23723
TheLuisAbarca <arclaav.adm@gmail.com>
2021-11-23 17:44:25 -0500

    Adding class functionality.

--- src/js/main.js
+++ src/js/main.js
@@ -2,34 +2,64 @@ const titleElement = document.querySelector('input[name="title2Add"');
 const authorElement = document.querySelector('input[name="author2Add"');
 const bookList = document.querySelector('#bookList');
 let books = [];
+let newArray;
 
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
-  this.cnt = bookList.childElementCount;
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 
-  this.addBook = (title, author) => {
+  addBook(title, author) {
+    let index = bookList.childElementCount;
     const bookElement = document.createElement('li');
-    bookElement.setAttribute('id', `book-${this.cnt}`);
+    bookElement.setAttribute('id', `book-${index}`);
     bookElement.innerHTML = ` <h2>${title}</h2>
                               <p>${author}</p>
-                              <button onClick=RemoveButton("${this.cnt}") type="submit">Remove</button>`;
+                              <button onClick=RemoveButton("${index}") type="submit">Remove</button>`;
     document.querySelector('#bookList').appendChild(bookElement);
   };
+/*
+  splice = (index, cnt) => {
+    for(let i = index; i< index + cnt; i++){ 
+      removeElement(i);
+    } 
+    t_this.splice(index.cnt)  
+  }
+*/
+  removeBook(index){
+    //document.querySelector(`#book-${index}`).remove();
+    const bookElement = document.querySelector(`#book-${index}`);
+    bookList.removeChild(bookElement);
+  }
 }
 
 function AddButton() { // eslint-disable-line no-unused-vars
   const genericbook = new Book(titleElement.value, authorElement.value);
-  books.push(genericbook);
+  books = books.concat(genericbook);
+  console.log(books);
   genericbook.addBook(titleElement.value, authorElement.value);
   localStorage.setItem('books', JSON.stringify(books));
+  //titleElement.value = '';
+  //authorElement.value = '';
 }
 
 function RemoveButton(index) { // eslint-disable-line no-unused-vars
-  const bookElement = document.querySelector(`#book-${index}`);
-  bookList.removeChild(bookElement);
-  books.splice(index, 1);
+  /*const bookElement = document.querySelector(`#book-${index}`);
+  bookList.removeChild(bookElement);*/
+  const genericbook = new Book(titleElement.value, authorElement.value);
+  genericbook.removeBook(index);
+  //books = books.splice(index, 1);
+  console.log(index);
+  //books.splice(index, 1);
+  console.log(books);
+  /*var filtered = books.filter(function(el) {
+    return el.cnt !== index;
+  });*/
+  //books = filtered;
+  //console.log(filtered);
   localStorage.setItem('books', JSON.stringify(books));
+  //localStorage.setItem('books', JSON.stringify(filtered));
 }
 
 function listBooks() {
@@ -47,4 +77,6 @@ function listBooks() {
 
 document.addEventListener('DOMContentLoaded', () => {
   listBooks();
+  /*const newBook = new Book(titleElement.value, authorElement.value, bookList.childElementCount);
+  newBook.addBook(titleElement.value, authorElement.value, bookList.childElementCount);*/
 });
