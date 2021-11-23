5f034360c958fcc41193c90d34d1d575d028b091
Daniel <danielufeli@yahoo.com>
2021-11-23 01:05:25 +0100

    Add Data to localstorage

--- src/js/main2.js
+++ src/js/main2.js
@@ -1,31 +1,34 @@
 let data = [];
 
-function addBook() {
+function bookFuction() {
   const obj = {
     title: document.getElementById("title").value,
     author: document.getElementById("author").value,
   };
   data.push(obj);
+  localStorage.setItem("books", JSON.stringify(data));
   document.getElementById("title").value = "";
   document.getElementById("author").value = "";
   disp();
 }
 
 function remove_element(index_no) {
-  let title = data.splice(index_no, 1);
+  data.splice(index_no, 1);
+  localStorage.setItem("books", JSON.stringify(data));
   disp();
 }
 
 function disp() {
+  let books = JSON.parse(localStorage.getItem("books"));
   let str = "";
-  str = "total number of books : " + data.length + "<br>";
-  for (i = 0; i < data.length; i++) {
+  str = "total number of books : " + books.length + "<br>";
+  for (i = 0; i < books.length; i++) {
     str += `
 <p>${i + 1}</p>
-<h2>${data[i].title}</h2>
-<p>${data[i].author}</p>
-<button onClick='remove_element("${data.indexOf(
-      data[i]
+<h2>${books[i].title}</h2>
+<p>${books[i].author}</p>
+<button onClick='remove_element("${books.indexOf(
+      books[i]
     )}")' type="submit">Remove</button>
 <br> <br>
 `;
@@ -33,3 +36,5 @@ function disp() {
 
   document.getElementById("bk").innerHTML = str;
 }
+
+disp();
