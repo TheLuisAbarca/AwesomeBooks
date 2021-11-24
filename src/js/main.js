4f44ccb38645e551de81fe6161054e638203d61b
TheLuisAbarca <arclaav.adm@gmail.com>
2021-11-24 09:49:24 -0500

    Fix some missusing things.

--- src/js/main.js
+++ src/js/main.js
@@ -1,88 +1,3 @@
-// const titleElement = document.querySelector('input[name="title2Add"');
-// const authorElement = document.querySelector('input[name="author2Add"');
-// const bookList = document.querySelector("#bookList");
-// let books = [];
-// let newArray;
-
-// class Book {
-//   constructor(title, author) {
-//     this.title = title;
-//     this.author = author;
-//   }
-
-//   addBook(title, author) {
-//     let index = bookList.childElementCount;
-//     const bookElement = document.createElement("li");
-//     bookElement.setAttribute("id", `book-${index}`);
-//     bookElement.innerHTML = ` <h2>${title}</h2>
-//                               <p>${author}</p>
-//                               <button onClick=RemoveButton("${index}") type="submit">Remove</button>`;
-//     document.querySelector("#bookList").appendChild(bookElement);
-//   }
-//   /*
-//   splice = (index, cnt) => {
-//     for(let i = index; i< index + cnt; i++){
-//       removeElement(i);
-//     }
-//     t_this.splice(index.cnt)
-//   }
-// */
-//   removeBook(index) {
-//     //document.querySelector(`#book-${index}`).remove();
-//     const bookElement = document.querySelector(`#book-${index}`);
-//     bookList.removeChild(bookElement);
-//   }
-// }
-
-// function AddButton() {
-//   // eslint-disable-line no-unused-vars
-//   const genericbook = new Book(titleElement.value, authorElement.value);
-//   books = books.concat(genericbook);
-//   console.log(books);
-//   genericbook.addBook(titleElement.value, authorElement.value);
-//   localStorage.setItem("books", JSON.stringify(books));
-//   //titleElement.value = '';
-//   //authorElement.value = '';
-// }
-
-// function RemoveButton(index) {
-//   // eslint-disable-line no-unused-vars
-//   /*const bookElement = document.querySelector(`#book-${index}`);
-//   bookList.removeChild(bookElement);*/
-//   const genericbook = new Book(titleElement.value, authorElement.value);
-//   // genericbook.removeBook(index);
-//   //books = books.splice(index, 1);
-//   // books.splice(index, 1);
-//   console.log(books);
-//   const filtered = books.filter((e, i) => {
-//     console.log(i);
-//     return i != index;
-//   });
-//   console.log(index);
-//   //books = filtered;
-//   //console.log(filtered);
-//   localStorage.setItem("books", JSON.stringify(filtered));
-//   //localStorage.setItem('books', JSON.stringify(filtered));
-// }
-
-// function listBooks() {
-//   const bookList = document.querySelector("#bookList");
-//   books = JSON.parse(localStorage.getItem("books"));
-//   if (!books) {
-//     books = [];
-//   }
-//   bookList.innerHTML = "";
-//   const genericbook = new Book(titleElement.value, authorElement.value);
-//   books.forEach((book) => {
-//     genericbook.addBook(book.title, book.author);
-//   });
-// }
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   listBooks();
-//   /*const newBook = new Book(titleElement.value, authorElement.value, bookList.childElementCount);
-//   newBook.addBook(titleElement.value, authorElement.value, bookList.childElementCount);*/
-// });
 const bookList = document.querySelector("#bookList");
 class Book {
   constructor(title, author) {
@@ -91,7 +6,7 @@ class Book {
   }
 
   static storeBooks(books) {
-    return localStorage.setItem("books", books);
+    return loca   lStorage.setItem("books", books);
   }
 
   static getBooks() {
