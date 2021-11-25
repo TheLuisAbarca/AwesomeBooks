acb05e315182e9f36f50a0838105bc55135b8977
TheLuisAbarca <arclaav.adm@gmail.com>
2021-11-25 17:26:10 -0500

    Fix Linters Errors.

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
-const btnBars = document.getElementById("barsBtn");
-const mobMenu = document.getElementsByClassName("mobMenu");
-const closeIcon = document.getElementsByClassName("closeIcon");
-const listEl = document.querySelectorAll(".mobMenu > ul > li");
-const bodyel = document.querySelector("body");
+const btnBars = document.getElementById('barsBtn');
+const mobMenu = document.getElementsByClassName('mobMenu');
+const closeIcon = document.getElementsByClassName('closeIcon');
+const listEl = document.querySelectorAll('.mobMenu > ul > li');
+const bodyel = document.querySelector('body');
 
-const bookList = document.querySelector("#bookList");
-const currentTime = document.querySelector(".currentTime");
+const bookList = document.querySelector('#bookList');
+const currentTime = document.querySelector('.currentTime');
+/* eslint-disable */
 const { DateTime } = luxon;
+/* eslint-enable */
 class Book {
   constructor(title, author) {
     this.title = title;
@@ -14,11 +16,11 @@ class Book {
   }
 
   static storeBooks(books) {
-    return localStorage.setItem("books", books);
+    return localStorage.setItem('books', books);
   }
 
   static getBooks() {
-    let books = JSON.parse(localStorage.getItem("books"));
+    let books = JSON.parse(localStorage.getItem('books'));
     if (!books) {
       books = [];
       return books;
@@ -30,14 +32,14 @@ class Book {
 
   static addBookMethod() {
     if (
-      document.querySelector("#author").value === "" ||
-      document.querySelector("#title").value === ""
+      document.querySelector('#author').value === ''
+      || document.querySelector('#title').value === ''
     ) {
-      alert("Please, fill both Author and Title."); // eslint-disable-line no-alert
+      alert('Please, fill both Author and Title.'); // eslint-disable-line no-alert
       return false;
     }
-    const title = document.querySelector("#title").value;
-    const author = document.querySelector("#author").value;
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
     return this.addBook(title, author);
   }
 
@@ -47,8 +49,8 @@ class Book {
       author,
     });
     this.storeBooks(JSON.stringify(this.data));
-    document.querySelector("#author").value = "";
-    document.querySelector("#title").value = "";
+    document.querySelector('#author').value = '';
+    document.querySelector('#title').value = '';
     return this.displayBook();
   }
 
@@ -60,10 +62,10 @@ class Book {
 
   static domCreateBook(title, author, index) {
     const eachBook = document.createDocumentFragment();
-    const eachList = document.createElement("li");
-    const eachTitle = document.createElement("h2");
-    const eachAuthor = document.createElement("p");
-    const removeBtn = document.createElement("button");
+    const eachList = document.createElement('li');
+    const eachTitle = document.createElement('h2');
+    const eachAuthor = document.createElement('p');
+    const removeBtn = document.createElement('button');
 
     eachBook.appendChild(eachList);
     eachList.id = `book-${index}`;
@@ -74,41 +76,40 @@ class Book {
 
     eachTitle.textContent = title;
     eachAuthor.textContent = author;
-    removeBtn.textContent = "Remove";
-    removeBtn.className = "rm-btn";
-    removeBtn.addEventListener("click", () => this.removeBook(index));
+    removeBtn.textContent = 'Remove';
+    removeBtn.className = 'rm-btn';
+    removeBtn.addEventListener('click', () => this.removeBook(index));
   }
 
   static displayBook() {
     this.listSection();
-    let bookDisplay = "";
+    let bookDisplay = '';
     if (this.data.length === 0) {
-      document.querySelector("#bookList").innerHTML =
-        '<p class="text-center mt-3">No Record Found</p>';
+      document.querySelector('#bookList').innerHTML = '<p class="text-center mt-3">No Record Found</p>';
     } else {
       this.data.map((e, i) => {
         bookDisplay += `<tr class="text-muted">
                         <th class="align-middle" scope="row">${i + 1}</th>
                           <td class="align-middle">"${this.data[i].title}" by ${
-          this.data[i].author
-        }</td>
+  this.data[i].author
+}</td>
                           <td class="cell">
                           <button class="btn border-3 text-white remove">
                           <i class="fas fa-trash"> Remove</i></button>
                           </td>
                         </tr>`;
 
-        document.querySelector("#bookList").innerHTML = bookDisplay;
+        document.querySelector('#bookList').innerHTML = bookDisplay;
 
-        const buttons = document.querySelectorAll("button.remove");
+        const buttons = document.querySelectorAll('button.remove');
         buttons.forEach((e, i) => {
           buttons[i].addEventListener(
-            "click",
+            'click',
             (event) => {
               Book.removeBook(Book.removeBook(this.data.indexOf(this.data[i])));
               event.preventDefault();
             },
-            false
+            false,
           );
         });
         return this.data;
@@ -117,7 +118,7 @@ class Book {
   }
 
   static getNumSuf(value) {
-    const numSuf = ["th", "st", "nd", "rd"];
+    const numSuf = ['th', 'st', 'nd', 'rd'];
     const lastDigit = value % 10;
     return numSuf[lastDigit] || numSuf[0];
   }
@@ -125,66 +126,67 @@ class Book {
   static getDateTime() {
     setInterval(() => {
       const currDT = DateTime.local();
-      currDT.loc.intl = "en-US";
+      currDT.loc.intl = 'en-US';
       const modCurrDT = currDT
         .toLocaleString({
           ...DateTime.DATETIME_MED_WITH_SECONDS,
-          month: "long",
+          month: 'long',
         })
-        .split(" ");
+        .split(' ');
       const dateNum = parseInt(modCurrDT[1], 10);
       modCurrDT[1] = dateNum + this.getNumSuf(dateNum);
-      modCurrDT[modCurrDT.length - 1] =
-        modCurrDT[modCurrDT.length - 1].toLowerCase();
-      currentTime.innerHTML = modCurrDT.join(" ");
+      modCurrDT[modCurrDT.length - 1] = modCurrDT[modCurrDT.length - 1].toLowerCase();
+      currentTime.innerHTML = modCurrDT.join(' ');
     }, 1000);
   }
 
   static toggleMenu() {
-    btnBars.addEventListener("click", () => {
-      mobMenu[0].style.display = "flex";
-      bodyel.style.overflow = "hidden";
+    btnBars.addEventListener('click', () => {
+      mobMenu[0].style.display = 'flex';
+      bodyel.style.overflow = 'hidden';
     });
 
-    closeIcon[0].addEventListener("click", () => {
-      mobMenu[0].style.display = "none";
-      bodyel.style.overflow = "auto";
+    closeIcon[0].addEventListener('click', () => {
+      mobMenu[0].style.display = 'none';
+      bodyel.style.overflow = 'auto';
     });
 
     listEl.forEach((el) => {
-      el.addEventListener("click", () => {
-        mobMenu[0].style.display = "none";
-        bodyel.style.overflow = "auto";
+      el.addEventListener('click', () => {
+        mobMenu[0].style.display = 'none';
+        bodyel.style.overflow = 'auto';
       });
     });
   }
 
   static addSection(name) {
-    let section = document.querySelector(`#${name}`);
-    return section.style.display = "block";
+    const section = document.querySelector(`#${name}`);
+    section.style.display = 'block';
+    return true;
   }
 
   static removeSection(name) {
-    let section = document.querySelector(`#${name}`);
-    return section.style.display = "none";
+    const section = document.querySelector(`#${name}`);
+    section.style.display = 'none';
+    return true;
   }
 
   static bookForm() {
-    this.removeSection("contactSection");
-    this.removeSection("listSection");
-    return this.addSection("bookForm");
+    this.removeSection('contactSection');
+    this.removeSection('listSection');
+    return this.addSection('bookForm');
   }
 
   static listSection() {
-    this.removeSection("bookForm");
-    this.removeSection("contactSection");
-    return this.addSection("listSection");
+    this.removeSection('bookForm');
+    this.removeSection('contactSection');
+    return this.addSection('listSection');
   }
 
   static contactSection() {
-    this.removeSection("listSection");
-    this.removeSection("bookForm");
-    return this.addSection("contactSection");
+    this.removeSection('listSection');
+    this.removeSection('bookForm');
+    return this.addSection('contactSection');
   }
 }
 Book.toggleMenu();
